771b8c16641a5018bdea1ba7b25052a84598e3df
oliceglad <vycheslavofficial@gmail.com>
2024-03-15 23:13:55 +0400

    added schemas for posts

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,16 @@
-"Schemas for database"
+from pydantic import BaseModel
+
+class BasePost(BaseModel):
+    tags: str
+    title: str
+    desc: str
+
+
+class PostIn(BasePost):
+    ...
+
+class PostOut(BasePost):
+    id: int
+
+    class Config:
+        orm_mode = True
