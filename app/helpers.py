9ee65a8f268be98c9a5fdbbdcd2e01eaa005285c
oliceglad <vycheslavofficial@gmail.com>
2024-03-18 21:06:26 +0400

    added new routes and changed helpers functions

--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,6 @@
-from typing import Dict, List
+from typing import Dict, List, Optional, Union
 from sqlalchemy.future import select
+from sqlalchemy import update
 
 from .database import engine, session, Base
 from .models import Post
@@ -9,6 +10,9 @@ async def to_start():
         await conn.run_sync(Base.metadata.create_all)
 
 async def to_shutdown():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
     await session.close()
     await engine.dispose()
 
@@ -24,3 +28,32 @@ async def find_all_posts_from_db()-> List[Post]:
     result = await session.scalars(select(Post))
     return [res for res in result]
 
+async def delete_post_by_year(year: int) -> Optional[Union[Post, None]]:
+    res = await session.scalars(select(Post).where(Post.year == int(year)))
+    first_res: Optional[Post] = res.first()
+
+    if first_res:
+        await session.delete(first_res)
+        return first_res
+
+    return None
+
+async def update_post_by_year(post: Dict) -> Optional[Union[Post, None]]:
+    stmt = (
+        update(Post).
+        where(Post.year == int(post['year'])).
+        values(post).
+        returning(Post)
+    )
+    result = await session.execute(stmt)
+    await session.commit()
+
+    return result.first()
+
+async def find_post_by_year(year: int) -> Optional[Union[Post, None]]:
+    res = await session.scalars(select(Post).where(Post.year == int(year)))
+    first_res: Optional[Post] = res.first()
+
+    if first_res:
+        return first_res
+    return None
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Path
-from typing import List, Dict, Any
+from typing import List, Optional, Union
 
 from app.models import Post
-from app.helpers import to_shutdown, to_start, add_new_post_to_db, find_all_posts_from_db
+from app.helpers import to_shutdown, to_start, add_new_post_to_db, find_all_posts_from_db, find_post_by_year,delete_post_by_year, update_post_by_year
 from app.schemas import PostIn, PostOut
 
 app = FastAPI()
@@ -21,15 +21,20 @@ async def add_new_post(post: PostIn) -> Post:
 
     return new_post
 
-@app.delete('/api/posts/{year}', response_model=PostOut)
-async def delete_year(year: int = Path(..., title="Year of the Post")) -> Post:
-    pass
+# @app.put('/api/posts', response_model=PostIn)
+# async def update_post(post: PostIn) -> Post:
+#     return await update_post_by_year(post.dict())
 
 @app.get('/api/posts', response_model=List[PostOut])
 async def get_all_posts() -> List[Post]:
     all_posts_info = await find_all_posts_from_db()
     return all_posts_info
 
-@app.get('api/years/{year}', response_model=PostOut)
-async def get_year(year: int = Path(..., title="Year of the Post")) -> Post:
-    pass
+@app.delete('/api/posts/{year}', response_model=PostOut)
+async def delete_post(year: int = Path(..., title="Year of the Post")) -> Post:
+    return await delete_post_by_year(year)
+
+@app.get('/api/posts/{year}', response_model=PostOut)
+async def get_post(year: int = Path(..., title="Year of the Post")) -> Optional[Union[Post, None]]:
+    res = await find_post_by_year(year)
+    return res
